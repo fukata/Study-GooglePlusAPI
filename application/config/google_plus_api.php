d9acfead2793a74fbf17f8e031c92647bc25cd62
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-09-16 10:20:35 +0900

    remove client_id,secret

--- application/config/google_plus_api.php
+++ application/config/google_plus_api.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-	'client_id' => '304781812250.apps.googleusercontent.com',
-	'client_secret' => 'eSyod0bRf-OzJzliTdy34RLM',
+	'client_id' => '',
+	'client_secret' => '',
 	'callback_url' => site_url('/oauth2callback')
 );
